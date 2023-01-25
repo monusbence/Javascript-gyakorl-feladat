33e5e9095daa7db67ca72742b44070c43dadfa6e
monusbence <72020098+monusbence@users.noreply.github.com>
2023-01-25 20:38:14 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -17,7 +17,7 @@ function harmadik(){
     var masodikoldal2 = document.getElementById("masodikoldal").value;
     document.getElementById("atfogo").value = Math.sqrt(Math.pow(elsooldal2,2) + Math.pow(masodikoldal2,2));
 }
-function random(){
+function randomos(){
     document.getElementById("elsooldal").value =  Math.random() * 100;
     document.getElementById("masodikoldal").value =  Math.random() * 100;
     document.getElementById("atfogo").value =  Math.random() * 100;
